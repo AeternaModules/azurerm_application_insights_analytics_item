d7f8a2b1819d4e499bab7d66e193605e1ba9b0c0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "application_insights_analytics_items" {
-  description = "All application_insights_analytics_item resources"
-  value       = azurerm_application_insights_analytics_item.application_insights_analytics_items
-}
 output "application_insights_analytics_items_application_insights_id" {
-  description = "List of application_insights_id values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.application_insights_id]
+  description = "Map of application_insights_id values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.application_insights_id }
 }
 output "application_insights_analytics_items_content" {
-  description = "List of content values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.content]
+  description = "Map of content values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.content }
 }
 output "application_insights_analytics_items_function_alias" {
-  description = "List of function_alias values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.function_alias]
+  description = "Map of function_alias values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.function_alias }
 }
 output "application_insights_analytics_items_name" {
-  description = "List of name values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.name]
+  description = "Map of name values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.name }
 }
 output "application_insights_analytics_items_scope" {
-  description = "List of scope values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.scope]
+  description = "Map of scope values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.scope }
 }
 output "application_insights_analytics_items_time_created" {
-  description = "List of time_created values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.time_created]
+  description = "Map of time_created values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.time_created }
 }
 output "application_insights_analytics_items_time_modified" {
-  description = "List of time_modified values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.time_modified]
+  description = "Map of time_modified values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.time_modified }
 }
 output "application_insights_analytics_items_type" {
-  description = "List of type values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.type]
+  description = "Map of type values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.type }
 }
 output "application_insights_analytics_items_version" {
-  description = "List of version values across all application_insights_analytics_items"
-  value       = [for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : v.version]
+  description = "Map of version values across all application_insights_analytics_items, keyed the same as var.application_insights_analytics_items"
+  value       = { for k, v in azurerm_application_insights_analytics_item.application_insights_analytics_items : k => v.version }
 }
 
